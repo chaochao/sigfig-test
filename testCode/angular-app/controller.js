90155819a82575852b1d52c0b75b783978987bf3
Chao Liu <liuchao8989@gmail.com>
2017-05-05 19:52:04 -0700

    add controller for each company

--- testCode/angular-app/controller.js
+++ testCode/angular-app/controller.js
@@ -1,12 +1,21 @@
 console.log('main controller');
-
 sigfigApp.controller('MainController', MainController)
+
 function MainController($scope, $http) {
   var self = this;
   $scope.title = 'scope main';
   self.title = 'self main';
   var companyUrl = '/companies'
-  $http.get(companyUrl).then(function(res){
+  $http.get(companyUrl).then(function(res) {
     console.log(res);
-  })
+  }).catch(function(err) {
+    console.log(err);
+  });
+}
+
+sigfigApp.controller('CompanyController', CompanyController)
+function CompanyController($scope, $http, $routeParams) {
+  var self = this;
+  var companyId = $routeParams.id;
+  console.log("cpid",companyId);
 }
--- testCode/angular-app/routes.js
+++ testCode/angular-app/routes.js
@@ -1,12 +1,16 @@
 sigfigApp.config(routes);
 
-function routes($routeProvider){
+function routes($routeProvider) {
   $routeProvider
-  .when('/',{
-    templateUrl:'angular-app/pages/main.html',
-    controller: 'MainController as vm'
-  })
-  .otherwise({
-    redirectTo: '/'
-  })
+    .when('/', {
+      templateUrl: 'angular-app/pages/main.html',
+      controller: 'MainController as vm'
+    })
+    .when('/companies/:id', {
+      templateUrl: 'angular-app/pages/company.html',
+      controller: 'CompanyController as vm'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
 }
